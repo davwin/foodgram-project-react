e57791ce7dfc13649f4999101a04a51024af42aa
David B <david.barsegyan5@gmail.com>
2022-11-07 23:25:47 +0300

    Fixing import

--- backend/foodgram/foods/management/commands/import_data.py
+++ backend/foodgram/foods/management/commands/import_data.py
@@ -21,5 +21,6 @@ class Command(BaseCommand):
 
         with open(path, 'r', encoding='utf-8') as f:
             Ingredient.objects.bulk_create(
-                objs=[Ingredient(**x) for x in json.load(f)], ignore_conflicts=True
+                objs=[Ingredient(**x) for x in json.load(f)],
+                ignore_conflicts=True
             )
